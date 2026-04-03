fab3c914c23687f7d7e5c92ec64d9748d997055d
onkar <onkar@wssllp.in>
2026-04-03 13:37:08 +0530

    mminor changes

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -9,6 +9,7 @@ const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
   secure: true,
+  Family:4,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS
